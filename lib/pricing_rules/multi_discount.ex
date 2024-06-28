0ee5015a91ca8a71cf7be4c11f5a72874d847798
Irena Ivanovska <irena@digitalpoint.mk>
2024-06-28 12:36:04 +0200

    Implementation of MultiDiscount

--- lib/pricing_rules/multi_discount.ex
+++ lib/pricing_rules/multi_discount.ex
@@ -9,5 +9,12 @@ defmodule SupermarketCashier.PricingRules.MultiDiscount do
       iex> MultiDiscount.apply_rule(3, 11.23)
       22.47
   """
-  def apply_rule(_count, _price), do: 0.0
+  def apply_rule(count, _price) when count >= 3 do
+    # 2/3 of 11.23 is approximately 7.49
+    7.49 * count
+  end
+
+  def apply_rule(count, price) do
+    price * count
+  end
 end
