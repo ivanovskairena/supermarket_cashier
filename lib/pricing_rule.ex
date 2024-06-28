277b49e57ca9d2f6c4ec257c268d9b3901562226
Irena Ivanovska <irena@digitalpoint.mk>
2024-06-28 15:40:11 +0200

    Refactor the code to pass the tests for pricing rules

--- lib/pricing_rule.ex
+++ lib/pricing_rule.ex
@@ -1,3 +1,12 @@
 defmodule SupermarketCashier.PricingRule do
-  @callback apply_rule(count :: integer, price :: float) :: float
+  @moduledoc """
+  Behavior definition for pricing rules.
+  """
+  @callback apply_rule(items :: list(), total :: float()) :: float()
+
+  def apply_rule(_items, total) do
+    total
+  end
+
+  def no_discount(_items, total), do: total
 end
