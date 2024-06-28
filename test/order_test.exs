f240550dc39c99af166b3f08c75082dcc582a5e5
Irena Ivanovska <irena@digitalpoint.mk>
2024-06-28 15:26:28 +0200

    Add tests for order module

--- test/order_test.exs
+++ test/order_test.exs
@@ -1,28 +1,17 @@
 defmodule SupermarketCashier.OrderTest do
   use ExUnit.Case
-  alias SupermarketCashier.{Order, Product}
 
-  describe "add_product/2" do
-    test "adds a product to the order" do
-      order = %Order{}
-      product = %Product{code: "GR1", name: "Green tea", price: 3.11}
+  alias SupermarketCashier.Order
 
-      assert order = Order.add_product(order, product)
-      assert length(order.items) == 1
-      assert Enum.at(order.items, 0) == product
-    end
-  end
+  test "creates a new order" do
+    pricing_rules = [
+      {SupermarketCashier.PricingRules.BuyOneGetOneFree, :apply},
+      {SupermarketCashier.PricingRules.BulkDiscount, :apply},
+      {SupermarketCashier.PricingRules.MultiDiscount, :apply}
+    ]
 
-  describe "total_price/1" do
-    test "calculates the total price of the order" do
-      order = %Order{}
-      product1 = %Product{code: "GR1", name: "Green tea", price: 3.11}
-      product2 = %Product{code: "SR1", name: "Strawberries", price: 5.00}
-
-      order = Order.add_product(order, product1)
-      order = Order.add_product(order, product2)
-
-      assert Order.total_price(order) == 8.11
-    end
+    order = Order.new(pricing_rules)
+    assert order.pricing_rules == pricing_rules
+    assert order.items == []
   end
 end
