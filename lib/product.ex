15011294a26f9783bb266672a0c86edefc87ce4e
Irena Ivanovska <irena@digitalpoint.mk>
2024-06-28 15:54:29 +0200

    Refactor product to pass tests

--- lib/product.ex
+++ lib/product.ex
@@ -21,7 +21,7 @@ defmodule SupermarketCashier.Product do
 
   defp products do
     [
-      %__MODULE__{code: "GR1", name: "Green tea", price: 3.11},
+      %__MODULE__{code: "GR1", name: "Green Tea", price: 3.11},
       %__MODULE__{code: "SR1", name: "Strawberries", price: 5.00},
       %__MODULE__{code: "CF1", name: "Coffee", price: 11.23}
     ]
