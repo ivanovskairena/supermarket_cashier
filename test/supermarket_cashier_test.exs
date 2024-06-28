15ffb36ade3810f951b9659568762b1a493a85e0
Irena Ivanovska <irena@digitalpoint.mk>
2024-06-28 12:15:03 +0200

    Adding Products and Calculating Total tests

--- test/supermarket_cashier_test.exs
+++ test/supermarket_cashier_test.exs
@@ -1,8 +1,25 @@
 defmodule SupermarketCashierTest do
   use ExUnit.Case
-  doctest SupermarketCashier
 
-  test "greets the world" do
-    assert SupermarketCashier.hello() == :world
+  describe "SupermarketCashier.add_product/2" do
+    test "adds a product to the cart" do
+      cart = []
+      product = %Product{code: "GR1", name: "Green Tea", price: 3.11}
+
+      assert SupermarketCashier.add_product(cart, product) == [
+               %Product{code: "GR1", name: "Green Tea", price: 3.11}
+             ]
+    end
+  end
+
+  describe "SupermarketCashier.total_price/1" do
+    test "calculates the total price of the cart" do
+      cart = [
+        %Product{code: "GR1", name: "Green Tea", price: 3.11},
+        %Product{code: "SR1", name: "Strawberries", price: 5.00}
+      ]
+
+      assert SupermarketCashier.total_price(cart) == 8.11
+    end
   end
 end
