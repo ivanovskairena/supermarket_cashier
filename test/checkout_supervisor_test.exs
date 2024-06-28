f3039b9e40429c0b7c9b0946f45969f635fdc388
Irena Ivanovska <irena@digitalpoint.mk>
2024-06-28 12:51:29 +0200

    Write a test for the Checkout Supervisor

---
+++ test/checkout_supervisor_test.exs
@@ -0,0 +1,13 @@
+defmodule SupermarketCashier.CheckoutSupervisorTest do
+  use ExUnit.Case
+  alias SupermarketCashier.CheckoutSupervisor
+
+  describe "checkout!/1" do
+    test "starts a new checkout process with valid pricing rules" do
+      {:ok, pid} = CheckoutSupervisor.start_link(name: __MODULE__)
+      pricing_rules = [{SupermarketCashier.PricingRules.BuyOneGetOneFree, :apply_rule}]
+      assert {:ok, checkout_pid} = CheckoutSupervisor.checkout!(pricing_rules)
+      assert Process.alive?(checkout_pid)
+    end
+  end
+end
