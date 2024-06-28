8f26c7bfffba39dfc55d18eea87036c181f219ad
Irena Ivanovska <irena@digitalpoint.mk>
2024-06-28 12:13:06 +0200

    adding necessary dependencies and setup

--- mix.exs
+++ mix.exs
@@ -5,9 +5,22 @@ defmodule SupermarketCashier.MixProject do
     [
       app: :supermarket_cashier,
       version: "0.1.0",
-      elixir: "~> 1.8",
+      elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ]
+    ]
+  end
+
+  def aliases do
+    [
+      test: ["credo --strict", "test"]
     ]
   end
 
@@ -21,8 +34,11 @@ defmodule SupermarketCashier.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:decimal, "~> 2.0"},
+      {:ex_doc, "~> 0.34", only: :dev, runtime: false},
+      {:credo, "~> 1.7.7", only: [:dev, :test], runtime: false},
+      {:excoveralls, "~> 0.18", only: :test},
+      {:dialyxir, "~> 1.4.3", only: [:dev], runtime: false}
     ]
   end
 end
